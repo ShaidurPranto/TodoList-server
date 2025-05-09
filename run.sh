b5927468395ba8d390ad0a60c520833c48d1c3c8
ShaidurPranto <mdshaidurrahmanpranto@gmail.com>
2025-05-09 11:42:32 +0600

    google client id , secret .env updated

--- run.sh
+++ run.sh
@@ -5,4 +5,6 @@ export FRONTEND_URL=http://localhost:5173
 
 SPRING_DATA_MONGODB_URI="mongodb+srv://pranto:admin@cluster0.lw1c8hg.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0" \
 SPRING_DATA_MONGODB_DATABASE=todoList \
+GOOGLE_CLIENT_ID=329814224341-gdcuhkd75fja16d28dp14sj8vif43lvg.apps.googleusercontent.com \
+GOOGLE_CLIENT_SECRET=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 \
 mvn spring-boot:run
